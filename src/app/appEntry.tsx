cd9d7ed76f901399fa87a4467b9fc519de02ea48
NastyaSulina <sulina01@mail.ru>
2025-07-30 18:14:56 +0300

    move browser router

--- src/app/appEntry.tsx
+++ src/app/appEntry.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import React from 'react'
 import { I18nextProvider } from 'react-i18next'
+import { BrowserRouter } from 'react-router-dom'
 
 import Routing from './appRouter'
 
@@ -16,8 +17,10 @@ root.render(
     <React.StrictMode>
         <I18nextProvider i18n={i18n}>
             <ThemeProvider>
-                <Routing />
-                <Message />
+                <BrowserRouter basename={process.env.NODE_ENV === 'production' ? '/wordle' : '/'}>
+                    <Routing />
+                    <Message />
+                </BrowserRouter>
             </ThemeProvider>
         </I18nextProvider>
     </React.StrictMode>,
--- src/app/appRouter.tsx
+++ src/app/appRouter.tsx
@@ -1,17 +1,15 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 import { MainPage } from '@/pages/MainPage'
 import { ErrorPage } from '@/pages/ErrorPage'
 
 const Routing = () => {
     return (
-        <BrowserRouter basename={process.env.NODE_ENV === 'production' ? '/wordle' : '/'}>
-            <Routes>
-                <Route path='/' element={<MainPage />} />
-                <Route path='/error' element={<ErrorPage />} />
-                <Route path='*' element={<Navigate to='/error' replace />} />
-            </Routes>
-        </BrowserRouter>
+        <Routes>
+            <Route path='/' element={<MainPage />} />
+            <Route path='/error' element={<ErrorPage />} />
+            <Route path='*' element={<Navigate to='/error' replace />} />
+        </Routes>
     )
 }
 
