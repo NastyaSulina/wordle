9f1180414c069931162bc23371de77f2c44213e4
NastyaSulina <sulina01@mail.ru>
2025-09-17 23:25:18 +0300

    update eslint and postcss configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,105 +9,78 @@ import globals from 'globals'
 import typescriptEslint from 'typescript-eslint'
 
 const eslintRecommended = eslint.configs.recommended
-const typescriptEslintRecommended = typescriptEslint.configs.recommended
 const importRecommended = importPlugin.flatConfigs.recommended
 const storybookRecommended = storybook.configs['flat/recommended']
+const tsRecommended = typescriptEslint.configs.recommended
 
-export default typescriptEslint.config(
+export default defineConfig([
+    globalIgnores(['dist/', 'node_modules/']),
+
     eslintRecommended,
-    typescriptEslintRecommended,
+    tsRecommended,
     importRecommended,
-    prettierRecommended,
     ...storybookRecommended,
-    defineConfig(
-        [globalIgnores(['dist/', 'node_modules/'])],
-        {
-            plugins: {
-                '@typescript-eslint': typescriptEslint.plugin,
-                'jsx-a11y': jsxA11y,
-                react,
-            },
+    prettierRecommended,
 
-            languageOptions: {
-                globals: {
-                    ...globals.browser,
-                },
+    {
+        plugins: {
+            '@typescript-eslint': typescriptEslint.plugin,
+            'jsx-a11y': jsxA11y,
+            react,
+        },
 
-                parser: typescriptEslint.parser,
-                ecmaVersion: 'latest',
-                sourceType: 'module',
-
-                parserOptions: {
-                    ecmaFeatures: {
-                        jsx: true,
-                    },
-
-                    project: './tsconfig.json', // Нужно для типозависимых правил, замедляет ESLint
-                },
-            },
-
-            settings: {
-                'import/resolver': {
-                    typescript: {},
-                },
-                react: {
-                    version: 'detect',
-                },
-            },
-
-            rules: {
-                'prettier/prettier': 2,
-                'import/order': [
-                    2,
-                    {
-                        groups: [
-                            'external',
-                            'builtin',
-                            'index',
-                            'sibling',
-                            'parent',
-                            'internal',
-                            'type',
-                        ],
-                        'newlines-between': 'always-and-inside-groups',
-                    },
-                ],
-                'import/no-extraneous-dependencies': ['off'],
-                'import/prefer-default-export': ['off'],
-
-                'react/require-default-props': ['off'],
-                'react/react-in-jsx-scope': ['off'],
-                'react/jsx-uses-react': ['off'],
-                'react/jsx-props-no-spreading': ['off'],
-                'react/no-unescaped-entities': ['off'],
-                'react/no-array-index-key': ['warn'],
-                'react/function-component-definition': [
-                    2,
-                    {
-                        namedComponents: 'arrow-function',
-                        unnamedComponents: 'arrow-function',
-                    },
-                ],
-
-                'jsx-a11y/alt-text': 'error',
-                'jsx-a11y/anchor-is-valid': 'error',
-                'jsx-a11y/label-has-associated-control': ['error', { assert: 'either' }],
-                'jsx-a11y/click-events-have-key-events': 'warn',
-
-                'no-plusplus': ['off'],
+        languageOptions: {
+            globals: { ...globals.browser },
+            parser: typescriptEslint.parser,
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            parserOptions: {
+                ecmaFeatures: { jsx: true },
             },
         },
-        {
-            files: ['.storybook/**/*', '**/*.stylelintrc.js', '**/*.config.js', '**/*.cjs'],
-            languageOptions: {
-                globals: {
-                    ...globals.node,
-                },
 
-                parserOptions: {
-                    project: null,
-                },
-            },
+        settings: {
+            'import/resolver': { typescript: {} },
+            react: { version: 'detect' },
         },
-    ),
-)
+
+        rules: {
+            'prettier/prettier': 'error',
+            'import/order': [
+                'error',
+                {
+                    groups: [
+                        'external',
+                        'builtin',
+                        'index',
+                        'sibling',
+                        'parent',
+                        'internal',
+                        'type',
+                    ],
+                    'newlines-between': 'always-and-inside-groups',
+                },
+            ],
+            'import/no-extraneous-dependencies': 'off',
+            'import/prefer-default-export': 'off',
+
+            'react/require-default-props': 'off',
+            'react/react-in-jsx-scope': 'off',
+            'react/jsx-uses-react': 'off',
+            'react/jsx-props-no-spreading': 'off',
+            'react/no-unescaped-entities': 'off',
+            'react/no-array-index-key': 'warn',
+            'react/function-component-definition': [
+                'error',
+                { namedComponents: 'arrow-function', unnamedComponents: 'arrow-function' },
+            ],
+
+            'jsx-a11y/alt-text': 'error',
+            'jsx-a11y/anchor-is-valid': 'error',
+            'jsx-a11y/label-has-associated-control': ['error', { assert: 'either' }],
+            'jsx-a11y/click-events-have-key-events': 'warn',
+
+            'no-plusplus': 'off',
+        },
+    },
+])
